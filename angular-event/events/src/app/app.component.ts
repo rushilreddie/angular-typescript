9ca49495c1f2e1fb0ac0119021e6e4258810e35d
LAPTOP-1GR06HCI\rajar <rushil.r.e@gmail.com>
2020-10-22 19:49:24 -0500

    Angular learning events

---
+++ angular-event/events/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+   title = 'Rushil';
+
+   months = ["January", "February", "March", "April",
+            "May", "June", "July", "August", "September",
+            "October", "November", "December"];
+   
+   isavailable = true;
+   myClickFunction(event) { 
+
+      alert("Button is clicked");
+      console.log(event);
+   }
+   changemonths(event) {
+      alert("Changed month from the Dropdown");
+   }
+}
